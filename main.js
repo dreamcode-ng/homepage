8dbb95736f9556a003e0ce06bda5b8c1ecbbdcdf
Juan Naranjo <juan.naranjo@dreamcodesoft.com>
2022-04-07 13:11:33 -0500

    correcion pagina blog

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const path = require("path")
 const fs = require("fs")
 
-const dirPathEN = path.join(__dirname, "../src/contentEN")
-const dirPathES = path.join(__dirname, "../src/contentES")
+
+const dirPathEN = path.join(__dirname, "../src/Articulos/contentEN")
+const dirPathES = path.join(__dirname, "../src/Articulos/contentES")
+const dirPathJob = path.join(__dirname, "../src/Jobs")
+
 
 let postlistES = []
 let postlistEN = []
 
+
+//--------------------------Articulos en Español--------------------------//
 
 const getPostsEN = () => {
     fs.readdir(dirPathEN, (err, files) => {
@@ -88,7 +93,7 @@ const getPostsEN = () => {
 }
 
 
-//--------------------------Articulos en Español--------------------------
+//--------------------------Articulos en Español--------------------------//
 
 const getPostsES = () => {
     fs.readdir(dirPathES, (err, files) => {
@@ -172,6 +177,92 @@ const getPostsES = () => {
     return 
 }
 
+//--------------------------Ofertas en Español--------------------------//
+/*
+const getJobs = () => {
+    fs.readdir(dirPathES, (err, files) => {
+        if (err) {
+            return console.log("Failed to list contents of directory: " + err)
+        }
+        files.forEach((file, i) => {
+
+
+
+            let obj = {}
+            let post
+            fs.readFile(`${dirPathES}/${file}`, "utf8", (err, contents) => {
+                const getMetadataIndices = (acc, elem, i) => {
+                    if (/^---/.test(elem)) {
+                        acc.push(i)
+                    }
+                    return acc
+                }
+                const parseMetadata = ({lines, metadataIndices}) => {
+                    if (metadataIndices.length > 0) {
+                        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
+                        metadata.forEach(line => {
+                            obj[line.split(": ")[0]] = line.split(": ")[1]
+                        })
+                        return obj
+                    }
+                }
+                const parseContent = ({lines, metadataIndices}) => {
+                    if (metadataIndices.length > 0) {
+                        lines = lines.slice(metadataIndices[1] + 1, lines.length)
+                    }
+                    return lines.join("\n")
+                }
+                const lines = contents.split("\n")
+                const metadataIndices = lines.reduce(getMetadataIndices, [])
+                const metadata = parseMetadata({lines, metadataIndices})
+                const content = parseContent({lines, metadataIndices})
+                const date = new Date(metadata.date)
+                const timestamp = date.getTime() / 1000
+
+                
+
+                post = {
+                    id: i + 1,
+                    title: metadata.title ? metadata.title : "No title given",
+                    metaTitle: metadata.metaTitle ? metadata.metaTitle : "No metaTitle given",
+                    metaDescription: metadata.metaDescription ? metadata.metaDescription : "No metaDescription given",
+                    read: metadata.read ? metadata.read : "No read given",
+                    date: metadata.date ? metadata.date : "No date given",
+                    url: metadata.url ? metadata.url : "No url given",
+                    imglink: metadata.imglink ? metadata.imglink : "No imglink given",
+                    category: metadata.category ? metadata.category : "No category given",
+                    content: content ? content : "No content given",
+                }
+    
+                console.log("ES: Id: " + post.id  +  " Url: " + post.url)
+
+                //console.log("<url><loc>http://www.dreamcodesoft.com/" + post.url + "/</loc><lastmod>2021-12-24T09:41:04+01:00</lastmod><priority>0.6</priority></url>")
+
+                postlistES.push(post)
+                if (i === files.length - 1) {
+                    //Orden de los post segun la fecha de creación
+                    const sortedList = postlistES.sort ((a, b) => {
+                        return a.id > b.id ? 1 : -1
+                    })
+                    //Crea el archivo json
+                    let data = JSON.stringify(sortedList)
+
+                 
+                    //fs.writeFileSync("src/assets/translations/fr.json", data)
+                    fs.writeFileSync("src/postsES.json", data)
+                }
+                
+            })
+        })
+    })
+
+
+
+    return 
+} */
+
 getPostsES()
 getPostsEN()
+
+//getJobs()
 
